1491d1fa82b4a9e53412c32509455e08e6639031
Theo Törnvall Hjortenmark <theo.tornvallhjorte@learnet.se>
2020-09-16 14:27:08 +0200

    yeet

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace metoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Heart of War!");
+
+            Console.WriteLine("Please enter your Warriors name");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Welcome " + name + " ,select your class");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ metoder/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace metoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DescribeLivingRoom();
+
+
+            Console.ReadLine();
+
+        }
+
+        static void DescribeLivingRoom()
+        {
+            Console.WriteLine("vardagsrummet är stort och mörkt");
+            Console.WriteLine("vardsrummet är stort och mörkt");
+            Console.WriteLine("vardagsrummet är st och mörkt");
+            Console.WriteLine("vardagsrummet är stort ocmörkt");
+            Console.WriteLine("vardagsrummet är stort och mörkt");
+            Console.WriteLine("vardagsrummet är  och mörkt");
+            Console.WriteLine("vadagsmmet är strt h mört");
+
+        }
+
+
+    }
+
+}
